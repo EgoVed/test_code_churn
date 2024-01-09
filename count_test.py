21f275b7ac63d71c48a4e3445bd80cbbb39d05f3
Egor Vedernikov <vedeegor@yandex.ru>
2024-01-09 23:14:46 +0300

    Update count_test.py
    add num3

--- count_test.py
+++ count_test.py
@@ -1,5 +1,6 @@
 num1 = 5.5
 num2 = 1.22
-sum = float(num1) + float(num2)
+num3 = 3.33
+sum = float(num1) + float(num2) + float(num3)
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
