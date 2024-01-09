ee36e21a2cf96190cc3b885f11ad2f05539b3b2e
Egor Vedernikov <vedeegor@yandex.ru>
2024-01-09 18:40:15 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 
 df = pd.read_csv('text.csv')
 count_values_1 = df['value'].count()
 
 sum = df['value'].sum()
-total = count_values_1/sum
+total_final = count_values_1/sum
+print(total_final)
