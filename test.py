2dfb7ceb97b36736e906ddaca062d9fd3a543969
Egor Vedernikov <vedeegor@yandex.ru>
2024-01-09 15:31:38 +0300

    Update test.py
    Add seaborn, del min-max, add total

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 df = pd.read_csv('text.csv')
-min = df['value'].min()
-max = df['value'].max()
 count_values_1 = df['value'].count()
 
-sum = abs(df['value'].sum())
+sum = df['value'].sum()
+total = count_values_1/sum
